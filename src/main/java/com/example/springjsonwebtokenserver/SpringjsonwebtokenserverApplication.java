0d2e9113c0de714aa54f4033ffc27492e65ceae7
johnhtml <jogerenag@unal.edu.co>
2022-06-11 20:16:17 -0500

    Initial commit

---
+++ src/main/java/com/example/springjsonwebtokenserver/SpringjsonwebtokenserverApplication.java
@@ -0,0 +1,16 @@
+package com.example.springjsonwebtokenserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjsonwebtokenserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjsonwebtokenserverApplication.class, args);
+	}
+
+	// This project was bootstrapped at
+	// https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.7.0&packaging=jar&jvmVersion=11&groupId=com.example&artifactId=springjsonwebtokenserver&name=springjsonwebtokenserver&description=Serve%20manager%20application%20with%20JSON%20web%20token&packageName=com.example.springjsonwebtokenserver&dependencies=web,mysql,validation,data-jpa,lombok
+	
+}
---
+++ src/test/java/com/example/springjsonwebtokenserver/SpringjsonwebtokenserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjsonwebtokenserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjsonwebtokenserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
